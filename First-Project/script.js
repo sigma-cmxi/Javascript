86f8dc0f5cbfc78b6f4caf4e7fed05577d51ed72
sigma-cmxi <sigma.cmxi@gmail.com>
2024-11-04 13:19:11 +0000

    day 2

--- First-Project/script.js
+++ First-Project/script.js
@@ -1,1 +1,10 @@
-document.getElementById("counter").innerHTML = 5
+let bonusPoints = 50 
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints  = bonusPoints + 45
+console.log(bonusPoints)
