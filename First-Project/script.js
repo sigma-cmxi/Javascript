23dab0f2e374a731ab7d5062610279e765444cc9
sigma-cmxi <sigma.cmxi@gmail.com>
2024-11-08 23:47:13 +0000

    day 4

--- First-Project/script.js
+++ First-Project/script.js
@@ -1,6 +1,20 @@
 let count = 0
+let draft = document.getElementById("draft-el")
 
-function countelog() {
-    count = count + 1 
+function increment() {
+    count = ++count
     document.getElementById("countertext").innerText = count 
 }
+
+function save(){
+    let countEl = " - " + count  
+    draft.innerText += " " + countEl
+}
+
+
+function reset() {
+    count = 0
+    document.getElementById("countertext").innerText = count 
+    draft.innerText = "drafts: "
+}
+
